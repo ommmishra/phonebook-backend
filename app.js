33f132ee4e22f35fd225fcce317c21fc4624b7cf
ommmishra <you@example.com>
2022-09-09 02:46:30 +0530

    modify serverless

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const router = require('./src/routes/router');
+const sls = require('serverless-http');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/', router);
+
+app.listen(2222)
+console.log('Server started at 2222')
